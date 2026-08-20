dcdf9e6a6aaaef7bbaf2944613b18432f0f1e622
Antonio Aranda <102337110+arandito@users.noreply.github.com>
2026-08-19 23:54:58 -0400

    Add CI and testing infra

--- scripts/docs/utils.py
+++ scripts/docs/utils.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """Utilities for reading service metadata from Smithy models."""
 
 import json
